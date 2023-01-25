d27af556bf487b841f60cc050ff417e0bca4c7ac
arar <chanpedrias@gmail.com>
2023-01-25 23:19:23 +0800

    Additional functionalities

--- url-shortener/url_shortener/shortener/admin.py
+++ url-shortener/url_shortener/shortener/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Url  # Url is the class function of models.py under shortener app
 
-# Register your models here.
+# Register your models here. This is for the admin web page
+admin.site.register(Url)
--- url-shortener/url_shortener/shortener/models.py
+++ url-shortener/url_shortener/shortener/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. These are fields in DB.
+class Url(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
